e3df66ee8f861942fac70f24417ed3bb81e7a394
topeakeredolu <tope.akeredolu@gmail.com>
2024-06-15 13:00:14 +0100

    solar

---
+++ src/test/java/main.java
@@ -0,0 +1,6 @@
+public class main {
+    public static void main(String[] args) {
+        solarsystembasicnew solarnew = new solarsystembasicnew();
+        solarnew.Moon="1";
+    }
+}
---
+++ src/test/java/solarsystembasicnew.java
@@ -0,0 +1,4 @@
+public class solarsystembasicnew {
+        String Sun;
+        String Moon;
+}
